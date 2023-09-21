1e259abdb0f9bc562f0dc548029f5055f872759a
Geniools <30665942+Geniools@users.noreply.github.com>
2023-09-21 19:02:50 +0200

    Started making the UI for the index page.

--- src/frontend/src/pages/IndexPage.js
+++ src/frontend/src/pages/IndexPage.js
@@ -1,15 +1,37 @@
 import React from "react";
 
-// Import the components
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import BackgroundImage from "../components/BackgroundImage";
+import {useNavigate} from "react-router-dom";
+import {USER_POSTS_URL} from "../UrlPaths";
 
-export default function IndexPage() {
+const IndexPage = () => {
+    const navigate = useNavigate();
+
+    const handleExploreNow = () => {
+        navigate(USER_POSTS_URL);
+    }
+
     return (
         <>
             <Header/>
-            <h1>Index page</h1>
+
+            <div className="content-wrapper">
+                <BackgroundImage src={'/static/assets/index_background.jpg'} children={
+                    <>
+                        <div className="dashboard-right-panel-header">
+                            <h1>IRentalize</h1>
+                        </div>
+
+                        <button onClick={handleExploreNow}>Explore now</button>
+                    </>
+                }/>
+            </div>
+
             <Footer/>
         </>
     );
 }
+
+export default IndexPage;
